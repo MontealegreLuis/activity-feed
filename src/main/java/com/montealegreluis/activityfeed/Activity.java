e8bd5cb8eccd889c254010ee5555cdf54e2d7de4
Luis Montealegre <montealegreluis@gmail.com>
2022-01-10 22:05:29 -0600

    feat: Optional context
    - Allow the context factory to be null

--- src/main/java/com/montealegreluis/activityfeed/Activity.java
+++ src/main/java/com/montealegreluis/activityfeed/Activity.java
@@ -1,5 +1,6 @@
 package com.montealegreluis.activityfeed;
 
+import com.montealegreluis.assertions.Assert;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -11,16 +12,32 @@ public final class Activity {
   private final ContextFactory factory;
   private final Map<String, Object> context = new LinkedHashMap<>();
 
+  public static Activity info(String identifier, String message) {
+    return info(identifier, message, null);
+  }
+
   public static Activity info(String identifier, String message, ContextFactory factory) {
     return new Activity(Level.INFO, identifier, message, factory);
+  }
+
+  public static Activity warning(String identifier, String message) {
+    return warning(identifier, message, null);
   }
 
   public static Activity warning(String identifier, String message, ContextFactory factory) {
     return new Activity(Level.WARNING, identifier, message, factory);
   }
 
+  public static Activity error(String identifier, String message) {
+    return error(identifier, message, null);
+  }
+
   public static Activity error(String identifier, String message, ContextFactory factory) {
     return new Activity(Level.SEVERE, identifier, message, factory);
+  }
+
+  public static Activity debug(String identifier, String message) {
+    return debug(identifier, message, null);
   }
 
   public static Activity debug(String identifier, String message, ContextFactory factory) {
@@ -34,7 +51,7 @@ public final class Activity {
   public Map<String, Object> context() {
     Map<String, Object> context = new LinkedHashMap<>();
     context.put("identifier", identifier);
-    factory.addEntries(context);
+    if (factory != null) factory.addEntries(context);
     this.context.put("context", context);
     return this.context;
   }
@@ -45,7 +62,9 @@ public final class Activity {
 
   private Activity(Level level, String identifier, String message, ContextFactory factory) {
     this.level = level;
+    Assert.notBlank(identifier, "Activity identifier cannot be blank. '%s' given");
     this.identifier = identifier;
+    Assert.notBlank(message, "Activity message cannot be blank. '%s' given");
     this.message = message;
     this.factory = factory;
   }
--- src/test/java/com/montealegreluis/activityfeed/ActivityTest.java
+++ src/test/java/com/montealegreluis/activityfeed/ActivityTest.java
@@ -2,6 +2,7 @@ package com.montealegreluis.activityfeed;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.montealegreluis.assertions.IllegalArgumentException;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
@@ -10,25 +11,69 @@ final class ActivityTest {
   void its_context_includes_its_identifier() {
     String userId = "5fa21160-f175-43dc-bac1-80d6e5d2d3bd";
     String identifier = "an-identifier";
-    var activity =
-        Activity.info(identifier, "A message", (context) -> context.put("userId", userId));
+    var activity = Activity.debug(identifier, "A message");
 
     var activityContext = activity.context();
 
     assertTrue(activityContext.containsKey("context"));
     @SuppressWarnings("unchecked")
     Map<String, Object> contextValues = (Map<String, Object>) (activityContext.get("context"));
+    assertEquals(1, contextValues.size());
+    assertTrue(contextValues.containsKey("identifier"));
+    assertEquals(identifier, contextValues.get("identifier"));
+  }
+
+  @Test
+  void its_adds_custom_values_to_its_context() {
+    var userId = "5fa21160-f175-43dc-bac1-80d6e5d2d3bd";
+    var correlationId = "9ea6ab93-f7ad-4497-a899-e6bc3938f3ba";
+    var identifier = "an-identifier";
+    var activity =
+        Activity.info(
+            identifier,
+            "A message",
+            (context) -> {
+              context.put("userId", userId);
+              context.put("correlationId", correlationId);
+            });
+
+    var activityContext = activity.context();
+
+    assertTrue(activityContext.containsKey("context"));
+    @SuppressWarnings("unchecked")
+    Map<String, Object> contextValues = (Map<String, Object>) (activityContext.get("context"));
+    assertEquals(3, contextValues.size());
     assertTrue(contextValues.containsKey("identifier"));
     assertEquals(identifier, contextValues.get("identifier"));
     assertTrue(contextValues.containsKey("userId"));
     assertEquals(userId, contextValues.get("userId"));
+    assertTrue(contextValues.containsKey("correlationId"));
+    assertEquals(correlationId, contextValues.get("correlationId"));
   }
 
   @Test
   void it_knows_its_message() {
     var message = "A message";
-    var activity = Activity.info("identifier", message, (context) -> context.put("key", "value"));
+    var error = Activity.error("identifier", message);
+    var info = Activity.info("identifier", message);
+    var warning = Activity.warning("identifier", message);
+    var debug = Activity.debug("identifier", message);
 
-    assertEquals(message, activity.message());
+    assertEquals(message, error.message());
+    assertEquals(message, info.message());
+    assertEquals(message, warning.message());
+    assertEquals(message, debug.message());
+  }
+
+  @Test
+  void it_prevents_blank_or_null_identifiers() {
+    assertThrows(IllegalArgumentException.class, () -> Activity.info(" ", "A message"));
+    assertThrows(IllegalArgumentException.class, () -> Activity.warning(null, "A message"));
+  }
+
+  @Test
+  void it_prevents_blank_or_null_messages() {
+    assertThrows(IllegalArgumentException.class, () -> Activity.info("identifier", " "));
+    assertThrows(IllegalArgumentException.class, () -> Activity.warning("identifier", null));
   }
 }
